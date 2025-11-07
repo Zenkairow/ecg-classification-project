dac4df551fbd87ecebad1fe8231424b3f30b18ae
Kirrmadaa <ayushpatil.in@gmail.com>
2025-11-07 16:15:37 +0530

    Fix: Correct GaussNoise param to std_range

--- data_synthesis/src/train_gan.py
+++ data_synthesis/src/train_gan.py
@@ -36,8 +36,7 @@ transform_pipeline = A.Compose(
     [
         # --- Heavy augmentations to create a "damaged" photo ---
 
-        # Corrected: Use 'var_limit'
-        A.GaussNoise(var_limit=(10.0, 50.0), p=0.8),
+        A.GaussNoise(std_range=(10.0, 50.0), p=0.8),
 
         A.GaussianBlur(blur_limit=(3, 7), p=0.8),
         A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.8),
